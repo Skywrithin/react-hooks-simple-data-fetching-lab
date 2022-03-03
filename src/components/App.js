81a2ce6024d7e5d2090e54027c586dd02abcfe40
Kaleb <kaleb.franken7@gmail.com>
2022-03-03 15:02:53 -0700

    commit tree 6dea17c48462403a454f70a7b1d7591808be630f

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import React, {useEffect, useState} from "react";
+import { data } from "../mocks/data";
+import { handlers } from "../mocks/handlers";
+
+
+function App() {
+
+    useEffect(() => {
+        const [isLoaded, setIsLoaded] = useState(false);
+        const [doggie, setDoggie] = useState(null)
+    q
+        fetch("https://dog.ceo/api/breeds/image/random")
+          .then((response) => response.json())
+          .then((data) => {
+            setDoggie(data)
+            setIsLoaded(true)
+          });
+      }, []);
+
+
+        if (!isLoaded){ return <p>Loading...</p>;}
+        return <img src={doggie} alt="A Random Dog"/>;
+
+}
+
+
+export default App;
